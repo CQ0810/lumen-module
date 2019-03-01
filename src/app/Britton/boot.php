8a109a0f67e75977d82c9956870160f45d7bd844
枫叶思源 <britton@126.com>
2019-03-01 17:13:24 +0800

    重命名文件

--- src/app/Britton/boot.php
+++ src/app/Britton/boot.php
@@ -2,7 +2,7 @@
 $app->routeMiddleware(['test' => \App\Contents\Http\Middleware\ExampleMiddleware::class]);
 
 $app->router->group([
-    'namespace' => 'App\Contents\Http\Controllers'
+    'namespace' => 'App\Britton\Http\Controllers'
 ], function ($router) use ($app) {
     (function (\Laravel\Lumen\Routing\Router $router, \Laravel\Lumen\Application $app) {
         $baseDir = [__DIR__ . '/Routes/admin', __DIR__ . '/Routes/mobile', __DIR__ . '/Routes/pcweb'];
